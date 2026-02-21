d4fbc159d470c458d0ff92e089241fc69fb82cfb
Parina Vaghadia <pvag7076@gmail.com>
2026-02-21 14:04:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # Load model
 # model.load(open("model.pkl", "rb"))
+model, imputer = pickle.load(open("model.pkl", "rb"))
 
 st.set_page_config(page_title="Air Pollution Level Detection", layout="centered")
 
