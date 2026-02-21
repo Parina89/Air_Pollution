2af00d75c572928883c39687ea6d31f57f49698b
Parina Vaghadia <pvag7076@gmail.com>
2026-02-21 12:39:02 +0530

    Comment out model loading functionality
    Comment out model loading code and related imports.

--- App.py
+++ App.py
@@ -3,12 +3,12 @@ import pickle
 import numpy as np
 import pandas as pd
 # import pickle
-from pathlib import Path
+# from pathlib import Path
 
-model_path = Path(__file__).parent / "model.pkl"
+# model_path = Path(__file__).parent / "model.pkl"
 
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
+# with open(model_path, "rb") as f:
+#     model = pickle.load(f)
 # Load model
 # model = pickle.load(open("model.pkl", "rb"))
 
