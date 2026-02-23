d55a6cdbc739801ff6f7d2a4e1bd00c97ebfe13b
Parina Vaghadia <pvag7076@gmail.com>
2026-02-23 12:04:27 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
